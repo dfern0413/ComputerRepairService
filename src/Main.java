161fccd6df0b1550fe87b23109503014be471486
dfern0413 <dfern0413@gmail.com>
2022-11-29 10:45:00 -0500

    Fixed Hierarchy, Add interfaces

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,13 @@ public class Main {
     public static void main(String[] args) {
         //Create Order
         Order order1 = new Order(1, "Mike", "Jeff", "keyboard", "DELL");
+        Order order2 = new Order(2, "Antonio", "Jeff", "speaker", "Sony");
         PartsCost partsCost = new PartsCost();
+        System.out.println(order1.toString());
+
+        if(order1.equals(order1)){
+            System.out.println("ERROR: Same Order");
+        }
 
         //Process
         String brokenPart;
--- src/order/Order.java
+++ src/order/Order.java
@@ -4,6 +4,8 @@ package order;
 import person.Employee;
 import person.Person;
 
+import java.util.Objects;
+
 public class Order implements IProcessOrder {
     int orderNumber;
     String customer;
@@ -25,5 +27,29 @@ public class Order implements IProcessOrder {
     @Override
     public String processOrder() {
         return part;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderNumber=" + orderNumber +
+                ", customer='" + customer + '\'' +
+                ", employee='" + employee + '\'' +
+                ", part='" + part + '\'' +
+                ", brand='" + brand + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return orderNumber == order.orderNumber && customer.equals(order.customer) && employee.equals(order.employee) && part.equals(order.part) && brand.equals(order.brand);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderNumber, customer, employee, part, brand);
     }
 }
